kubernetes/kubernetes

ignore replicaset-replicas-before-scale annotation in deployment rollbacks

staging/src/k8s.io/kubectl/pkg/polymorphichelpers/rollback.go
@@ -182,12 +182,13 @@ func equalIgnoreHash(template1, template2 *corev1.PodTemplateSpec) bool {
 // annotationsToSkip lists the annotations that should be preserved from the deployment and not
 // copied from the replicaset when rolling a deployment back
 var annotationsToSkip = map[string]bool{
-	corev1.LastAppliedConfigAnnotation:       true,
-	deploymentutil.RevisionAnnotation:        true,
-	deploymentutil.RevisionHistoryAnnotation: true,
-	deploymentutil.DesiredReplicasAnnotation: true,
-	deploymentutil.MaxReplicasAnnotation:     true,
-	appsv1.DeprecatedRollbackTo:              true,
+	corev1.LastAppliedConfigAnnotation:           true,
+	deploymentutil.RevisionAnnotation:            true,
+	deploymentutil.RevisionHistoryAnnotation:     true,
+	deploymentutil.DesiredReplicasAnnotation:     true,
+	deploymentutil.MaxReplicasAnnotation:         true,
+	deploymentutil.ReplicaSetReplicasBeforeScale: true,
+	appsv1.DeprecatedRollbackTo:                  true,
 }
 
 // getPatch returns a patch that can be applied to restore a Deployment to a

losewinner/k8s

staging/src/k8s.io/kubectl/pkg/polymorphichelpers/rollback.go
@@ -182,12 +182,13 @@ func equalIgnoreHash(template1, template2 *corev1.PodTemplateSpec) bool {
 // annotationsToSkip lists the annotations that should be preserved from the deployment and not
 // copied from the replicaset when rolling a deployment back
 var annotationsToSkip = map[string]bool{
-	corev1.LastAppliedConfigAnnotation:       true,
-	deploymentutil.RevisionAnnotation:        true,
-	deploymentutil.RevisionHistoryAnnotation: true,
-	deploymentutil.DesiredReplicasAnnotation: true,
-	deploymentutil.MaxReplicasAnnotation:     true,
-	appsv1.DeprecatedRollbackTo:              true,
+	corev1.LastAppliedConfigAnnotation:                     true,
+	deploymentutil.RevisionAnnotation:                      true,
+	deploymentutil.RevisionHistoryAnnotation:               true,
+	deploymentutil.DesiredReplicasAnnotation:               true,
+	deploymentutil.MaxReplicasAnnotation:                   true,
+	deploymentutil.ReplicaSetReplicasBeforeScaleAnnotation: true,
+	appsv1.DeprecatedRollbackTo:                            true,
 }
 
 // getPatch returns a patch that can be applied to restore a Deployment to a